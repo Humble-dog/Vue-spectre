673a18ef6c1f9d38bf55bdbade1c3b84b709c636
Tex_wz <tangzhilin6@outlook.com>
2018-11-20 15:25:57 +0800

    Add form

---
+++ src/components/elements/Form.vue
@@ -0,0 +1,52 @@
+<template>
+  <form>
+    <div class="form-group" v-for="(input, i) in form" :key="i">
+      <template v-if="input.type === 'radio'">
+        <label class="form-label">{{input.title}}</label>
+        <label
+          v-for="(option, optionIndex) in input.options" :key="input.field + '-' + optionIndex"
+          class="form-radio"
+          :class="{ 'form-inline': input.inline }">
+          <input type="radio"
+          :name="input.field"
+          :checked="input.checked !== undefined ? input.checked == optionIndex : false">
+          <i class="form-icon"></i> {{option}}
+        </label>
+      </template>
+      <template v-if="input.type === 'switch'">
+        <label class="form-switch" :class="{ 'form-inline': input.inline }">
+          <input type="checkbox">
+          <i class="form-icon"></i> {{input.title}}
+        </label>
+      </template>
+      <template v-if="input.type === 'checkbox'">
+        <label class="form-checkbox" :class="{ 'form-inline': input.inline }">
+          <input type="checkbox" :checked="input.checked">
+          <i class="form-icon"></i> {{input.title}}
+        </label>
+      </template>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    form: [
+      {
+        type: 'radio',
+        title: 'Gender',
+        field: 'gender',
+        checked: 0,
+        inline: true,
+        options: ['male', 'female']
+      },
+      {
+        type: 'checkbox',
+        title: 'Helo world',
+        checked: true
+      }
+    ]
+  })
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'spectre.css'
+import '../node_modules/spectre.css/src/spectre.scss'
+import '../node_modules/spectre.css/src/spectre-exp.scss'
+import '../node_modules/spectre.css/src/spectre-icons.scss'
 
 import button from './components/elements/Button.vue'
 import label from './components/elements/Label.vue'
 import table from './components/elements/Table.vue'
+import form from './components/elements/Form.vue'
 import code from './components/elements/Code.vue'
 import icon from './components/elements/Icon.vue'
 
@@ -12,6 +15,7 @@ const Components = {
   button,
   label,
   table,
+  form,
   code,
   icon
 }
