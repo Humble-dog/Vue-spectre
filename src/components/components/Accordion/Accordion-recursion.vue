5a973f534f1826de61f2a1e5807c929452d886ef
DavidTan0527 <david_weiucheng@hotmail.com>
2018-11-22 15:26:11 +0800

    Update accordion

--- src/components/components/Accordion/Accordion-recursion.vue
+++ src/components/components/Accordion/Accordion-recursion.vue
@@ -2,34 +2,26 @@
   <div class="accordion-children">
     <div class="accordion-item"
     v-for="(item, index) in data" :key="item.title">
-      <div class="parent" @click="Array.isArray(active) 
-      ? active.indexOf(index) == -1 ? active.push(index) : active.filter((value, index, arr) => {
-        return value == index;
-      })
-      : active == index ? active = -1 : active = index" style="cursor: pointer;">
+      <div class="parent" @click="Array.isArray(active)
+      ? active.indexOf(index) === -1 ? active.push(index) : active.splice(active.indexOf(index), 1)
+      : active === index ? active = -1 : active = index" style="cursor: pointer;">
         <i class="icon" v-if="icon"
         :class="
-        Array.isArray(active) 
-        ? active.indexOf(index) != -1 ? 'icon-arrow-down' : 'icon-arrow-right'
-        : active == index ? 'icon-arrow-down' : 'icon-arrow-right'"></i>
+        Array.isArray(active)
+        ? active.indexOf(index) !== -1 ? 'icon-arrow-down' : 'icon-arrow-right'
+        : active === index ? 'icon-arrow-down' : 'icon-arrow-right'"></i>
         {{item.title}}
       </div>
-      <template v-if="item.image && !item.children &&
-      Array.isArray(active) 
-      ? active.indexOf(index) != -1 ? true : false
-      : active == index ? true : false">
+      <template v-if="showContent(item, index, 'image')">
         <img :src="item.image">
       </template>
-      <template v-if="item.content && !item.children &&
-      Array.isArray(active) 
-      ? active.indexOf(index) != -1 ? true : false
-      : active == index ? true : false">
+      <template v-if="showContent(item, index, 'content')">
         <div>{{item.content}}</div>
       </template>
       <accordionItem class="children" style="cursor: pointer; position: relative; left: .5rem;"
       v-if="item.children && Array.isArray(active)
-      ? active.indexOf(index) != -1
-      : active == index"
+      ? active.indexOf(index) !== -1
+      : active === index"
       :data="item.children" :icon="icon" :multiOpen="multiOpen" />
     </div>
   </div>
@@ -38,15 +30,15 @@
 <script>
 export default {
   name: 'Accordion',
-  mounted() {
+  mounted () {
     if (this.multiOpen) {
-      this.active = [];
+      this.active = []
     } else {
-      this.active = -1;
+      this.active = -1
     }
   },
   data: () => ({
-    active: ''
+    active: null
   }),
   props: {
     data: Array,
@@ -56,11 +48,23 @@ export default {
     },
     multiOpen: {
       type: Boolean,
-      default: false,
+      default: false
     }
   },
   components: {
     accordionItem: () => import('./Accordions')
+  },
+  methods: {
+    showContent (item, index, type = 'content') {
+      var found = false
+      if (Array.isArray(this.active) && this.active.indexOf(index) !== -1) {
+        found = true
+      } else if (this.active === index) {
+        found = true
+      }
+      if (type === 'image') { return item.image && !item.children && found }
+      if (type === 'content') { return item.content && !item.children && found }
+    }
   }
 }
 </script>
