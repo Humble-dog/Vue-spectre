85526da3876fac93f38674a7ef4566590d294b6b
DavidTan0527 <david_weiucheng@hotmail.com>
2018-11-24 15:03:24 +0800

    Linting

--- src/components/components/Breadcrumbs.vue
+++ src/components/components/Breadcrumbs.vue
@@ -4,9 +4,9 @@
       <li class="breadcrumb-item" v-for="(item, index) in data" :key="index">
         <a :href="item.href" v-if="item.href">{{item.text}}</a>
 
-        <router-link :to="item.to" 
+        <router-link :to="item.to"
         v-else-if="item.to">{{item.text}}</router-link>
-        
+
         <span v-else>{{item.text}}</span>
       </li>
     </ul>
