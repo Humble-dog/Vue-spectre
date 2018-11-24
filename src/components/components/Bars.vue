a3fa7fe6f84d9cfe503b1e293edeefea2304cb4f
DavidTan0527 <david_weiucheng@hotmail.com>
2018-11-24 10:38:12 +0800

    Linting

--- src/components/components/Bars.vue
+++ src/components/components/Bars.vue
@@ -3,16 +3,17 @@
     thin   ? 'bar-sm' : '',
     slider ? 'bar-slider' : ''
   ]">
-  
+
     <!-- Ranged Slider -->
-    <div class="bar-item" role="progressbar" :style="'width:'+end.value+'%;'"
-    v-if="slider && data.length === 2"
-    v-for="end in data" :key="end.value">
-      <button class="bar-slider-btn btn" role="slider"
-      :class="[
-        tooltip ? 'tooltip' : '',
-        tooltipPosition ? 'tooltip-'+tooltipPosition : ''
-      ]" :data-tooltip="tooltip ? end.value : ''"></button>
+    <div class="bar-item-wrapper" v-if="slider && data.length === 2">
+      <div class="bar-item" role="progressbar" :style="'width:'+end.value+'%;'"
+      v-for="end in data" :key="end.value">
+        <button class="bar-slider-btn btn" role="slider"
+        :class="[
+          tooltip ? 'tooltip' : '',
+          tooltipPosition ? 'tooltip-'+tooltipPosition : ''
+        ]" :data-tooltip="tooltip ? end.value : ''"></button>
+      </div>
     </div>
 
     <!-- Slider -->
@@ -26,13 +27,15 @@
     </div>
 
     <!-- Multi -->
-    <div class="multi bar-item" :class="[
-      tooltip ? 'tooltip' : '',
-      tooltipPosition ? 'tooltip-'+tooltipPosition : '',
-      'bar-item-'+(index+1)
-    ]" :data-tooltip="item.tooltip" :style="'width:'+item.value+'%;'" 
-    v-if="multi" v-for="(item, index) in data" :key="index">
-      {{item.content || item.value}}
+    <div class="bar-item-wrapper" v-if="multi">
+      <div class="multi bar-item" :class="[
+        tooltip ? 'tooltip' : '',
+        tooltipPosition ? 'tooltip-'+tooltipPosition : '',
+        'bar-item-'+(index+1)
+      ]" :data-tooltip="item.tooltip" :style="'width:'+item.value+'%;'"
+      v-for="(item, index) in data" :key="index">
+        {{item.content || item.value}}
+      </div>
     </div>
 
     <!-- Single -->
@@ -40,7 +43,7 @@
       tooltip ? 'tooltip' : '',
       tooltipPosition ? 'tooltip-'+tooltipPosition : '',
     ]" :data-tooltip="data.tooltip || data.value" role="progressbar" :style="'width:'+(data.value-range[0])/(range[1] - range[0])*100+'%;'"
-    :aria-valuenow="data.value" :aria-valuemin="range[0]" :aria-valuemax="range[1]" 
+    :aria-valuenow="data.value" :aria-valuemin="range[0]" :aria-valuemax="range[1]"
     v-if="!multi && !slider">
       <div v-if="!noContent">{{data.content || data.value}}</div>
     </div>
@@ -54,10 +57,10 @@ export default {
     range: {
       type: Array,
       default: () => {
-        return [0, 100];
+        return [0, 100]
       },
       validator: function (thing) {
-        return thing.length === 2 && thing[0] < thing [1]
+        return thing.length === 2 && thing[0] < thing[1]
       }
     },
     thin: {
